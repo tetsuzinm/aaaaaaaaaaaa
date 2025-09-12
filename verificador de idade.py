baf86699a25c1aa6d499e1631506d850e3f928c2
tetsuzinm <mphelippe201104@gmail.com>
2025-09-12 09:31:19 -0300

    Update verificador de idade.py

--- verificador de idade.py
+++ verificador de idade.py
@@ -4,3 +4,12 @@ idade = int(input("Qual a sua idade: "))
 verificador =  idade >= 18
 
 print(verificador)
+
+
+#Para dirigir a pessoa tem que ser igual ou maior que 18 anos de idade e ter a carteira de motorista
+
+idade = int(input("Qual a sua idade: "))
+carterira = True
+verificador = idade >= 18 and carterira
+
+print(verificador)
