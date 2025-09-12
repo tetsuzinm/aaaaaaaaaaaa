184acafbc0b2c8e0d7b115ed3649b4199c0984b1
tetsuzinm <mphelippe201104@gmail.com>
2025-09-12 12:28:44 -0300

    Update verificador de idade.py

--- verificador de idade.py
+++ verificador de idade.py
@@ -13,3 +13,15 @@ carterira = True
 verificador = idade >= 18 and carterira
 
 print(verificador)
+
+#segunda versão
+
+idade = int(input('Qual sua idade? '))
+autorizacao_pais = input('Você tem autorização dos seus pais? (sim/não) ')
+
+if idade >= 18:
+    print('Você pode entrar na festa.')
+elif idade >= 16 and autorizacao_pais == 'sim':
+    print('Você pode entrar na festa com autorização dos pais.')
+else:
+    print('Você não pode entrar na festa.')
